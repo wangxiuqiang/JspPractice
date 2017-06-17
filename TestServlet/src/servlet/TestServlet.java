3d01490eeb99cebf6fcdf0d965404af26675da7a
wangxiuqiang <wangxiuqianga@163.com>
2017-06-17 16:40:05 +0800

    ServletTest

---
+++ TestServlet/src/servlet/TestServlet.java
@@ -0,0 +1,29 @@
+package servlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        System.out.println("用get方式请求servlet");// 控制台打印输出
+        PrintWriter out=response.getWriter();
+        response.setContentType("text/html;charset=utf-8");
+        out.println("<strong>get de servlet</strong><hr>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        System.out.println("用post方式请求servlet");//控制台打印输出
+        PrintWriter out=response.getWriter();
+        response.setContentType("text/html;charset=utf-8");
+        out.println("<strong>postservlet</strong><hr>");
+    }
+
+}
---
+++ TestServlet/web/index.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+         pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <title>Servlet</title>
+</head>
+<body>
+<h1>servletTest</h1>
+<a href="servlet/TestServlet">TestGet</a>
+<form action="servlet/TestServlet" method="post">
+  <input type="submit" value="post方式">
+</form>
+</body>
+</html>
